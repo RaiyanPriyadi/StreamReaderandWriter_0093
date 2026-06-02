30f639d8d3ab988922a1289c815f6ddf1f5a5637
RaiyanPriyadi <dwi.raiyan.ft25@mail.umy.ac.id>
2026-06-02 16:27:23 +0700

    kondisi jika file ada

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -39,3 +39,8 @@ int main()
 
     //menunjuk ke sebuah file dengan ekstensi .txt
     infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
