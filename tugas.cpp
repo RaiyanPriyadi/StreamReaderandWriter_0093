4ffaa2f393f61409bd5d0320635ef9554d67f8b4
RaiyanPriyadi <dwi.raiyan.ft25@mail.umy.ac.id>
2026-06-02 16:04:54 +0700

    first commit

---
+++ tugas.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <array>
+#include <vector>
+#include <stdexcept>
+
+using namespace std;
+
+// 1. Struktur Kelas TokoElektronik
+class TokoElektronik {
+private:
+    // Atribut Private: Array berkapasitas 3 elemen
+    array<string, 3> etalase;
+
+public:
+    // Constructor: Mengisi data awal ke dalam array
+    TokoElektronik() {
+        etalase[0] = "Laptop Asus ROG";
+        etalase[1] = "Monitor Samsung 27 inch";
+        etalase[2] = "Keyboard Mechanical";
+    }
+
+    // Method Public dengan parameter size_t
+    string ambilBarang(size_t nomorRak) {
+        try {
